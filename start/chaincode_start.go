3a449a62180365b0e47b1d78e05a8136a4de7618
hyperledger1 <saithrinath.g@gmail.com>
2017-01-23 18:19:27 +0530

    Update chaincode_start.go
    laptop creation is done by using init_laptop

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -19,12 +19,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"encoding/json"
+	"time"
+	"strings"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+         "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+
+type Laptop struct{
+	ID string `json:"id"`					//the fieldtags are needed to keep case from bouncing around
+	User string `json:"user"`
+        RAM  string `json:"ram"`
+        ROM string `json:"rom"`
 }
 
 // ============================================================================================================================
@@ -43,7 +55,12 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	return nil, nil
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+return nil, nil
 }
 
 // Invoke is our entry point to invoke a chaincode function
@@ -54,6 +71,9 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
 	}
+	 else if function == "init_laptop" {									//create a new marble
+		return t.init_laptop(stub, args)
+}
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -64,11 +84,70 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" { //read a variable
+		return t.read(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function)
 
-	return nil, errors.New("Received unknown function query: " + function)
+return nil, errors.New("Received unknown function query: " + function)
+}
+
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+
+func (t *SimpleChaincode) init_laptop(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+
+	//   0       1       2     3
+	// "1x22354", "sarath", "4GB", "1TB"
+	if len(args) != 4 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	}
+
+
+	fmt.Println("Starting init laptop")
+	id := args[0]
+	user := args[1]
+	ram := args[2]
+	rom := args[3]
+
+	//check if laptop already exists
+	laptopAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return nil, errors.New("Failed to get marble name")
+	}
+	del := Laptop{}
+	json.Unmarshal(marbleAsBytes, &del)
+	if del.ID == id{
+		fmt.Println("This laptop arleady exists in the network: " + id)
+		fmt.Println(del);
+		return nil, errors.New("This laptop arleady exists")		
+	}
+	
+	//build the laptop  json string manually
+	str := `{"id": "` + id + `", "user": "` + user + `", "ram": ` + ram + `, "rom": "` + rom + `"}`
+	err = stub.PutState(id, []byte(str))									//store marble with id as key
+	if err != nil {
+		return nil, err
+	}
+		
+	fmt.Println("- end init marble")
+	return nil, nil
 }
